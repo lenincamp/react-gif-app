a08e9b2d5a99e9526ad149de02550338db48a062
lcampoverde <lenincamp18@gmail.com>
2020-07-11 13:06:08 -0500

    [REF]: change name gifapp

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -7,7 +7,7 @@ export const GifExpertApp = () => {
 
   return (
     <>
-      <h2>GifExpertApp</h2>
+      <h2>GifApp</h2>
       <AddCategory setCategories={setCategories} />
       <hr />
       <ol>
