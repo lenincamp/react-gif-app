d6beff4795bceb3b7abfe68b0ed35130d9f24a01
lcampoverde <lenincamp18@gmail.com>
2020-07-11 12:44:08 -0500

    first commit

---
+++ src/helpers/getGif.js
@@ -0,0 +1,15 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=Co9mHsk9bqatahJRyqU4B0S8RTNBvW4L`;
+
+  const respuesta = await fetch(url);
+  const { data } = await respuesta.json();
+  const gifs = data.map(({ id, title, images }) => ({
+    id,
+    title,
+    url: images?.downsized_medium.url,
+  }));
+  console.log(data, gifs);
+  return gifs;
+};
