77f91f6873c27e7760ec268516e56ecc9658cd32
lcampoverde <lenincamp18@gmail.com>
2020-07-11 12:48:15 -0500

    [ADD]: docs

---
+++ docs/precache-manifest.b369d48b3acf5de14605e69fbb4bfda7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f29408979c9dc455f4fe90d01530cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6215a04f24d63ad692d3",
+    "url": "/static/css/main.db200a6d.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6215a04f24d63ad692d3",
+    "url": "/static/js/main.8c4e2e68.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
